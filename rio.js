c78ed72d722d7141b4a56c48ebc4b547cfeb6cc0
GUNAVIJAY01 <gunanathan0@gmail.com>
2023-09-16 17:57:00 +0530

    update

---
+++ rio.js
@@ -0,0 +1,66 @@
+// 1.What is react?
+    //    ->React is a open source library .
+    //    ->React is a userinterface.
+    //    ->React is tool for building Components. 
+   
+//   2.What is component? 
+            // ->component are independent and reusable bit of Code.
+            // ->There are two types
+            //         ->Function Components
+            //         ->Class Components
+
+//  3.Write function syntax.
+    // import React from 'react'
+    // const Fn=()=>{
+    //     return(<div>
+
+    //     </div>)
+
+    // }       
+    // export default Fn;
+
+//  4.Write class syntax.	
+    // import React, { Component } from 'react'
+    // class Cl extends Component{
+    //     render(){
+    //         return(<div>
+
+    //         </div>)
+    //     }
+    // }    
+    // export default Cl
+
+//  5. Write example of internal style.
+
+    // let arry={Color:"red"}
+    // <div>
+    //     <h2 style={arry}>hello</h2>
+    //     </div>
+
+//  6. What is JSX.  
+
+    // ->JSX-javascript syntax extension
+    // ->JSX is a combination of html and Javascript.
+    // ->JSX Create the element inside the javascript 
+
+//  7. What are the advantages of react?    
+
+// ->JSX (JavaScript Syntax Extension)
+// ->single page web pplication.
+// ->code is reduced.
+// ->itis declarative.
+
+//  8.Difference between real dom and virtual dom.
+
+// ->Real DOM
+            // ->DOM makes the performance Slower, not because the processing itself is done in more time.
+            //    ->More memory usage  
+// ->Virtual Dom        
+            //  ->Virtual DOM makes the performance faster, not because the processing itself is done in less time.   
+            // ->No memory usage
+
+//  9. Write any three packages
+
+    // ->npm i react-slick
+    // ->npm i slick-carousel
+    // ->npm i react-icons
